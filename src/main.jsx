c1a0887e62baad4d24a0a2c6d3f51a33ebefd2f5
varshneypiyush19 <varshneypiyush2004@gmail.com>
2024-11-24 18:39:14 +0530

    server changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./styles/app.scss";
 import { createContext } from "react";
 import AppWrapper from "./AppWrapper";
 
-export const server = "https://nodejs-todo-h9cl.onrender.com/api/v1";
+export const server = "https://backend-todo-985l.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
